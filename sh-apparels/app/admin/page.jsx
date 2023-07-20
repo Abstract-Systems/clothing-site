d19e7738483ee432e4f4ccc4559b233d0fcb8b49
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2023-07-20 22:56:54 +0500

    Admin route

---
+++ sh-apparels/app/admin/page.jsx
@@ -0,0 +1,48 @@
+// components/ProtectedContent.js
+"use client"
+import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import Sidebar from '../components/AdminSidebar';
+
+export default function ProtectedContent() {
+  const { data: session, status } = useSession(
+    {
+        required: true,
+        onUnauthenticated() {
+            redirect('/api/auth/signin?callbackUrl=/admin')
+        }
+
+    }
+
+  );
+  const router = useRouter();
+  const allowedEmails = ["hasaanahmad10023@gmail.com", "email"]
+
+  useEffect(() => {
+    // Redirect to the root page when the user is not authenticated
+    if (status === 'unauthenticated' || (session && !allowedEmails.includes(session.user.email))) {
+      router.push('/');
+    }
+  }, [status]);
+
+  if (status === 'loading') {
+    // Handle loading state if needed
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'authenticated' && session?.user?.email && allowedEmails.includes(session.user.email)) {
+    return (
+        <Sidebar/>
+    );
+  }
+
+//   unauthorized should be returned to home page
+    return (
+        <div>
+
+        </div>
+    )
+
+}
