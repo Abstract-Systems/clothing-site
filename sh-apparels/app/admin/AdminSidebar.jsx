432f8a0e24eddf5b1d29e743eb34fcd42039bbd2
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2023-07-21 10:18:13 +0500

    Admin Sidebar and add a product

---
+++ sh-apparels/app/admin/AdminSidebar.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { AddProduct } from './AddProduct';
+
+const Sidebar = () => {
+  const [activeOption, setActiveOption] = useState(1);
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      const key = event.key;
+      if (parseInt(key) >= 1 && parseInt(key) <= 4) {
+        setActiveOption(parseInt(key));
+      }
+    };
+
+    document.addEventListener('keypress', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  }, []);
+
+  // Helper function to render the content based on activeOption
+  const renderContent = () => {
+    switch (activeOption) {
+      case 1:
+        return <AddProduct />;
+      case 2:
+        return <ManageCategories />;
+      case 3:
+        return <ManageProducts />;
+      case 4:
+        return <Orders />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="flex">
+      <div className="bg-gray-800 text-white h-screen w-1/6 py-8 px-4">
+        <h2 className="text-xl font-bold mb-4">Sidebar</h2>
+        <ul>
+          <li
+            className={`mb-2 p-2 cursor-pointer ${
+              activeOption === 1 ? 'bg-blue-600' : ''
+            }`}
+            onClick={() => setActiveOption(1)}
+          >
+            Add Product
+          </li>
+          <li
+            className={`mb-2 p-2 cursor-pointer ${
+              activeOption === 2 ? 'bg-blue-600' : ''
+            }`}
+            onClick={() => setActiveOption(2)}
+          >
+            Manage Categories
+          </li>
+          <li
+            className={`mb-2 p-2 cursor-pointer ${
+              activeOption === 3 ? 'bg-blue-600' : ''
+            }`}
+            onClick={() => setActiveOption(3)}
+          >
+            Manage Products
+          </li>
+          <li
+            className={`mb-2 p-2 cursor-pointer ${
+              activeOption === 4 ? 'bg-blue-600' : ''
+            }`}
+            onClick={() => setActiveOption(4)}
+          >
+            Orders
+          </li>
+        </ul>
+      </div>
+      <div className="flex-1 p-4">{renderContent()}</div>
+    </div>
+  );
+};
+
+export default Sidebar;
