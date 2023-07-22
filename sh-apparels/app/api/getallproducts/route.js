f12723a81df9d6fa548d73bf00eaf9314a99f12a
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2023-07-22 10:32:14 +0500

    get and post api

---
+++ sh-apparels/app/api/getallproducts/route.js
@@ -0,0 +1,28 @@
+import connectDB from "@/libs/connectDB";
+import { NextResponse } from "next/server";
+import Product from "@/models/Product";
+
+export async function POST(request){
+    const {title, Description, slug, price, images, category, stock} = await request.json();
+    await connectDB();
+    Product.create({
+        title,
+        Description,
+        slug,
+        price,
+        images,
+        category,
+        stock,
+    })
+    return NextResponse.json({message: "Product created successfully"})
+}
+
+
+export async function GET(){
+    await connectDB();
+    const products = await Product.find({});
+    return NextResponse.json(products);
+
+}
+
+
