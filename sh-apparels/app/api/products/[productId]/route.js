8dba8bc06eb8a54039ef39bcdd70643b403c25b1
Haider12212 <84367962+Haider12212@users.noreply.github.com>
2023-07-23 21:36:52 +0500

    Co-authored-by: Hasaan Ahmad <HasaanAhmad@users.noreply.github.com>

--- sh-apparels/app/api/products/[productId]/route.js
+++ sh-apparels/app/api/products/[productId]/route.js
@@ -1,63 +1,39 @@
 import connectDB from "@/libs/connectDB";
 import Product from "@/models/Product";
 
-export default async function handler(req, res) {
-  // Extract the productId from the request query
+export async function patchProductHandler(req, res) {
   const { productId } = req.query;
 
-  // Handle different HTTP methods using a switch statement
-  switch (req.method) {
-    // Handle the PATCH request to update product stock
-    case "PATCH":
-      // Extract the stock from the request body
-      const { stock } = req.body;
-      try {
-        // Connect to the database
-        await connectDB();
+  // Handle the PATCH request to update product stock
+  const { stock } = req.body;
+  try {
+    await connectDB();
+    const product = await Product.findByIdAndUpdate(productId, { stock }, { new: true });
 
-        // Find the product by ID and update its stock
-        const product = await Product.findByIdAndUpdate(
-          productId,
-          { stock },
-          { new: true } // Return the updated product after the update
-        );
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
 
-        // Check if the product was found and updated successfully
-        if (!product) {
-          return res.status(404).json({ message: "Product not found" });
-        }
-
-        // Respond with a success message
-        return res.status(200).json({ message: "Product stock updated successfully" });
-      } catch (error) {
-        // Handle any internal server errors
-        return res.status(500).json({ message: "Internal Server Error" });
-      }
-
-    // Handle the DELETE request to delete the product
-    case "DELETE":
-      try {
-        // Connect to the database
-        await connectDB();
-
-        // Find the product by ID and delete it
-        const product = await Product.findByIdAndDelete(productId);
-
-        // Check if the product was found and deleted successfully
-        if (!product) {
-          return res.status(404).json({ message: "Product not found" });
-        }
-
-        // Respond with a success message
-        return res.status(200).json({ message: "Product deleted successfully" });
-      } catch (error) {
-        // Handle any internal server errors
-        return res.status(500).json({ message: "Internal Server Error" });
-      }
-
-    default:
-      // Handle requests with unsupported HTTP methods
-      return res.status(405).json({ message: "Method Not Allowed" });
+    return res.status(200).json({ message: "Product stock updated successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
+export async function deleteProductHandler(req, res) {
+  const { productId } = req.query;
+
+  // Handle the DELETE request to delete the product
+  try {
+    await connectDB();
+    const product = await Product.findByIdAndDelete(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    return res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
