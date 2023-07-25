db66f11b2c4f5ece558a9b51293ddb9e51fed78f
Haider12212 <84367962+Haider12212@users.noreply.github.com>
2023-07-25 08:40:50 +0500

    modules

--- sh-apparels/app/components/CartDropdown.jsx
+++ sh-apparels/app/components/CartDropdown.jsx
@@ -4,34 +4,6 @@ import { XMarkIcon } from "@heroicons/react/24/outline";
 import { useContext } from "react";
 import { CartContext } from "@/context/CartContext";
 
-const products = [
-  {
-    id: 1,
-    name: "Throwback Hip Bag",
-    href: "#",
-    color: "Salmon",
-    price: "$90.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
-    imageAlt:
-      "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
-  },
-  {
-    id: 2,
-    name: "Medium Stuff Satchel",
-    href: "#",
-    color: "Blue",
-    price: "$32.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
-    imageAlt:
-      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
-  },
-  // More products...
-];
-
 export default function CartDropdown({ setOpen }) {
   const { cart, removeFromCart, increaseQuantity, decreaseQuantity } = useContext(CartContext);
   const calculateTotalPrice = () => {
