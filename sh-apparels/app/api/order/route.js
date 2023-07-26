409714dc155398f27515e6926226d6c22316af1b
Haider12212 <84367962+Haider12212@users.noreply.github.com>
2023-07-26 11:01:58 +0500

    some changes

--- sh-apparels/app/api/order/route.js
+++ sh-apparels/app/api/order/route.js
@@ -39,3 +39,22 @@ export async function GET() {
   const orders = await Order.find({});
   return NextResponse.json(orders);
 }
+export async function PUT(request) {
+  const data = await request.json();
+  const { _id } = data;
+
+  try {
+    await connectDB(); // Make sure this function connects to the MongoDB database
+    const updateOrder = await Order.findByIdAndUpdate(_id, data, { new: true });
+    if(!updateOrder){
+      return NextResponse.json({message: "Order not found"}, 404);
+    }
+    return NextResponse.json({message: "Order updated successfully", updateOrder});
+  } catch (error) {
+    // Handle the error appropriately, e.g., logging or sending an error response
+    console.error(error);
+    return NextResponse.json({ message: "Error updating order", error }, 500);
+  }
+}
+  
+
