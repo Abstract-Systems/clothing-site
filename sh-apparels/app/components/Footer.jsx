8e507a3e30900e33d02e20424b0ac4094c8ba05c
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2023-07-27 15:56:46 +0500

    few fixes

--- sh-apparels/app/components/Footer.jsx
+++ sh-apparels/app/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import logo from '../../public/logo.png';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
@@ -8,8 +10,8 @@ const Footer = () => {
           <div className="sm:flex sm:items-center sm:justify-between">
             {/* Footer content goes here */}
             <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
-              <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          <Image src={logo} alt="logo" width={50} height={50} />
+              <span className="self-center text-2xl pl-5 font-semibold whitespace-nowrap dark:text-white">Horizon Wear</span>
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
               <li>
@@ -27,7 +29,7 @@ const Footer = () => {
             </ul>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">Horizon Wear   ™</a>. All Rights Reserved.</span>
         </div>
       </footer>
     </div>
